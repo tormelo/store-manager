5c45641e12bfc6e8c8d3820f0f9ddf7918ab0bf4
tormelo <23294699+tormelo@users.noreply.github.com>
2022-11-11 18:19:24 -0300

    implementa router para endpoint /products

---
+++ src/routers/products.router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { productsController } = require('../controllers');
+
+const router = express.Router();
+
+router.get(
+  '/',
+  productsController.listProducts,
+);
+
+router.get(
+  '/:id',
+  productsController.getProduct,
+);
+
+module.exports = router;
