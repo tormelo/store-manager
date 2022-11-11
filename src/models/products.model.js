10302f88e733a32ff1d719903262e2029de6cd5c
tormelo <23294699+tormelo@users.noreply.github.com>
2022-11-11 16:30:55 -0300

    fix listAll query, ordenando por id

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -2,7 +2,7 @@ const connection = require('./connection');
 
 const listAll = async () => {
   const [result] = await connection.execute(
-    'SELECT * FROM StoreManager.products',
+    'SELECT * FROM StoreManager.products ORDER BY id',
   );
   return result;
 };
