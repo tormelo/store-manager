f91aea884282e4b25e6153f264dcf4cc68766233
tormelo <23294699+tormelo@users.noreply.github.com>
2023-07-29 15:24:38 -0300

    chore: Add database restore script and package.json option

---
+++ StoreManager.sql
@@ -0,0 +1,54 @@
+DROP DATABASE IF EXISTS StoreManager;
+
+CREATE DATABASE StoreManager;
+
+USE StoreManager;
+
+CREATE TABLE products (
+  id INT NOT NULL auto_increment,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY(id)
+) ENGINE=INNODB;
+
+CREATE TABLE sales (
+  id INT NOT NULL auto_increment,
+  date DATETIME DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY(id)
+) ENGINE=INNODB;
+
+CREATE TABLE sales_products (
+  sale_id INT NOT NULL,
+  product_id INT NOT NULL,
+  quantity INT NOT NULL,
+  CONSTRAINT PRIMARY KEY (sale_id , product_id),
+  FOREIGN KEY (sale_id)
+    REFERENCES sales (id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (product_id)
+    REFERENCES products (id)
+    ON DELETE CASCADE
+)  ENGINE=INNODB;
+
+SET SQL_SAFE_UPDATES = 0;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+TRUNCATE TABLE `products`;
+TRUNCATE TABLE `sales`;
+TRUNCATE TABLE `sales_products`;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+INSERT INTO StoreManager.products (name) VALUES
+    ("Martelo de Thor"),
+    ("Traje de encolhimento"),
+    ("Escudo do Capitão América");
+
+INSERT INTO StoreManager.sales (date) VALUES
+    (NOW()),
+    (NOW());
+
+INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) VALUES
+    (1, 1, 5),
+    (1, 2, 10),
+    (2, 3, 15);
